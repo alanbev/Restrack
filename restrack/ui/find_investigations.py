88f0ab4fb592c01bb4fd2450f3171542608c80e8
alanbev <alanjbeveridge@outloook.com>
2024-10-15 17:27:00 +0100

    fifteen October

--- restrack/ui/find_investigations.py
+++ restrack/ui/find_investigations.py
@@ -1,3 +1,8 @@
-import panel
+import panel as pn
 from restrack.data_access.find_orders import orders_for_patient 
-panel.extension(sizing_mode="stretch_width", template="fast")
+pn.extension(sizing_mode="stretch_width", template="fast")
+hosp_numb = pn.widgets.IntInput(name='Hospital number')
+button = pn.widgets.Button()
+pn.Row(hosp_numb,button).servable()
+
+investigation_list= orders_for_patient(hosp_numb)
