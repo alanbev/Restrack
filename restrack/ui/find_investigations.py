d459ae7a2dd4f2fc4cf35ea8ad0996f5d5527187
alanbev <alanjbeveridge@outloook.com>
2024-10-20 16:39:46 +0100

    reactive table in find_investigations

--- restrack/ui/find_investigations.py
+++ restrack/ui/find_investigations.py
@@ -1,8 +1,29 @@
 import panel as pn
 from restrack.data_access.find_orders import orders_for_patient 
-pn.extension(sizing_mode="stretch_width", template="fast")
-hosp_numb = pn.widgets.IntInput(name='Hospital number')
-button = pn.widgets.Button()
-pn.Row(hosp_numb,button).servable()
+pn.extension('tabulator',sizing_mode="stretch_width", template="fast")
+display_table=pn.panel("table goes here")
 
-investigation_list= orders_for_patient(hosp_numb)
+def show_table(event):
+    print(event)
+    table = pn.widgets.Tabulator(orders_for_patient(hosp_numb.value))
+    table=pn.panel(table)
+    print (table)
+    return table
+
+   
+
+hosp_numb = pn.widgets.TextInput(name='Hospital number')
+enter_hosp_no = pn.widgets.Button(name="Enter",button_type='primary' )
+
+result=pn.bind(show_table, hosp_numb.param.value) 
+
+def clickhandler(clicked):
+    if clicked:
+        return result
+    return ""
+
+display_table= pn.panel(pn.bind(clickhandler,enter_hosp_no))
+pn.Column(
+pn.Row(hosp_numb, enter_hosp_no),
+pn.Row(display_table)
+).servable()
